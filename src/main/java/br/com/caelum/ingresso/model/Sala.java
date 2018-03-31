7e3b129ae5a6a7c1042f6b3a20f743d368a096cb
Thiago de Oliveira <thiago_slipknot123@hotmail.com>
2018-03-31 16:19:48 -0300

    Desconto Ingresso

--- src/main/java/br/com/caelum/ingresso/model/Sala.java
+++ src/main/java/br/com/caelum/ingresso/model/Sala.java
@@ -3,6 +3,9 @@ package br.com.caelum.ingresso.model;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,65 +15,81 @@ import java.util.stream.Collectors;
 @Entity
 public class Sala {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
+	@Id
+	@GeneratedValue
+	private Integer id;
 
-    @NotBlank
-    private String nome;
+	@NotBlank
+	private String nome;
 
-    @OneToMany(fetch = FetchType.EAGER)
-    private List<Lugar> lugares = new ArrayList<>();
+	private BigDecimal preco;
 
-    /**
-     * @deprecated hibernate only
-     */
-    public Sala() {
+	@OneToMany(fetch = FetchType.EAGER)
+	private List<Lugar> lugares = new ArrayList<>();
 
-    }
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sala() {
 
-    public Sala(String nome) {
-        this.nome = nome;
-    }
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Sala(String nome, BigDecimal preco) {
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+		this.nome = nome;
+		this.preco = preco;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
+	public String getNome() {
+		return nome;
+	}
 
-    public void add(Lugar lugar) {
-        this.lugares.add(lugar);
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public List<Lugar> getLugares() {
-        return lugares;
-    }
+	public void add(Lugar lugar) {
+		this.lugares.add(lugar);
+	}
 
-    public void setLugares(List<Lugar> lugares) {
-        this.lugares = lugares;
-    }
+	public List<Lugar> getLugares() {
+		return lugares;
+	}
 
-    public Map<String, List<Lugar>> getMapaDeLugares() {
-        if(!this.lugares.isEmpty()){
-            return this.lugares.stream().collect(Collectors.groupingBy(Lugar::getFileira,Collectors.toList()));
-        }
-        return Collections.emptyMap();
-    }
+	public void setLugares(List<Lugar> lugares) {
+		this.lugares = lugares;
+	}
 
-    public Integer lugar(String fileira, Integer posicao){
-        Optional<Lugar> optional = this.lugares.stream().filter((x) -> fileira.equals(x.getFileira()) && posicao.equals(x.getPosicao())).findFirst();
-        return optional.get().getId();
-    }
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		if (!this.lugares.isEmpty()) {
+			return this.lugares.stream().collect(Collectors.groupingBy(Lugar::getFileira, Collectors.toList()));
+		}
+		return Collections.emptyMap();
+	}
+
+	public Integer lugar(String fileira, Integer posicao) {
+		Optional<Lugar> optional = this.lugares.stream()
+				.filter((x) -> fileira.equals(x.getFileira()) && posicao.equals(x.getPosicao())).findFirst();
+		return optional.get().getId();
+	}
+
+	public BigDecimal getPreco() {
+
+		return preco.setScale(2, RoundingMode.HALF_UP);
+
+	}
+
+	public void setPreco(BigDecimal preco) {
+
+		this.preco = preco;
+	}
+
 }
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -15,6 +17,8 @@ public class Sessao {
 	private Integer id;
 	private LocalTime horario;
 
+	private BigDecimal preco;
+
 	@ManyToOne
 	private Sala sala;
 
@@ -26,6 +30,7 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public LocalTime getHorarioTermino() {
@@ -67,6 +72,17 @@ public class Sessao {
 
 	public Sessao() {
 
+	}
+
+	public BigDecimal getPreco() {
+
+		return preco.setScale(2, RoundingMode.HALF_UP);
+
+	}
+
+	public void setPreco(BigDecimal preco) {
+
+		this.preco = preco;
 	}
 
 }
